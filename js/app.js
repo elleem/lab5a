ee9e69791cbb8d510e34cf88c8168d4955e449d4
Lauren Main <lauren.main28@gmail.com>
2022-05-14 17:25:54 -0700

    solve prob c

--- js/app.js
+++ js/app.js
@@ -50,12 +50,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b, c) {//eslint-disable-line
+ let m = sum(a,b)[0];
+ let sumOne = sum(m,c)[0];
 
+ let n = multiply(a,b)[0];
+ let productOne = multiply(n,c)[0];
+
+ return [sumOne, productOne,`${a} and ${b} and ${c} sum to ${sumOne}.`,`The product of ${a} and ${b} and ${c} is ${productOne}.`]
 }
-
+//console.log(sumAndMultiply(4,7,5));
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
