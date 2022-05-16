61993e9e787a64275d4591cbee62b9832e60bd91
Lauren Main <lauren.main28@gmail.com>
2022-05-15 22:05:16 -0700

    solve 5

--- js/app.js
+++ js/app.js
@@ -105,12 +105,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+function multiplyArray(multArr) { 
+  //eslint-disable-line
+  let multiply=1;
+  for (let i = 0; i < testArray.length; i++) {
+     multiply *= testArray[i];
+  } 
+   return [multiply, `The numbers ${testArray} have a product of ${multiply}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -133,11 +138,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+function multiplyAnyArray(dynamicArray) { 
+  //eslint-disable-line
+let length= testDynamicArray.length;
+for (let 1 = 0; i < length; i++) {
+ testDynmaicArray[i] *=1;
+}
+return [`The numbers ${testDynamicArray} have a product of ${}`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
